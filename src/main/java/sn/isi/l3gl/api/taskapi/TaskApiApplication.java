eb6ea2d90cd312d42134e2e7f7dca72c9905546c
Makhoumetzo <makhtardiiop@gmail.com>
2026-02-26 19:10:28 +0000

    chore: configuration finale docker et ansible

--- src/main/java/sn/isi/l3gl/api/taskapi/TaskApiApplication.java
+++ src/main/java/sn/isi/l3gl/api/taskapi/TaskApiApplication.java
@@ -2,12 +2,17 @@ package sn.isi.l3gl.api.taskapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"sn.isi.l3gl.api.taskapi", // packages de task-api
+		"sn.isi.l3gl.core.taskcore" // packages de task-core (TaskService, etc.)
+})
+@EntityScan(basePackages = "sn.isi.l3gl.core.taskcore.entity")
+@EnableJpaRepositories(basePackages = "sn.isi.l3gl.core.taskcore.repository")
 public class TaskApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApiApplication.class, args);
 	}
-
 }
